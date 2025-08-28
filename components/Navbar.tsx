afd1e30e9aca275fd3a984ec07f0069850cc3664
Bhupendra <bhupendralute1234@gmail.com>
2025-08-28 23:19:30 +0530

    Navbar logo converted to homepage link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between fixed top-0 w-full py-2 px-4 bg-background z-50">
-        <div className="inline-flex justify-center items-center p-1">
+        <Link href="/" className="inline-flex justify-center items-center p-1">
           <Image className="size-8 md:size-10 " src="/favicon.webp" alt="logo" width={100} height={100} />
           <span className="text-xl md:text-2xl text-foreground font-semibold">AgreeWise</span>
-        </div>
+        </Link>
     </nav>
   )
 }
